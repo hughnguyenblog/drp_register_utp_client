b9733194309c8396507eabfa8521c5b015e42b70
hyunguyen1305 <huy.nguyenngoc1305@gmail.com>
2020-10-20 23:24:46 +0700

    utd_client

---
+++ src/components/SignUp.js
@@ -0,0 +1,198 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+
+import Grid from "@material-ui/core/Grid";
+
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+import Swal from "sweetalert2";
+
+import axios from "axios";
+import queryString from "query-string";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    formName: "INTERNATIONAL",
+    fieldOptionalText: "(optional)",
+    firstName: "",
+    lastName: "",
+    fullName: "test_dr1",
+    email: "",
+    country: "VIE",
+    zip: "50000",
+    city: "HoChiMinhCity",
+    specialty: "1",
+    practiceType: "9",
+    registrationUserName: "",
+    password: "",
+    verifyPassword: "",
+  });
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = {
+      formName: "INTERNATIONAL",
+      firstName: values.firstName,
+      lastName: values.lastName,
+      email: values.email,
+      country: "VIE",
+      zip: "50000",
+      city: "HoChiMinhCity",
+      specialty: "1",
+      practiceType: "9",
+      registrationUserName: values.registrationUserName,
+      password: values.password,
+      verifyPassword: values.password,
+    };
+    console.log(formData)
+    const url =
+      "http://localhost:3000/registerUptodate";
+    axios
+      .post(url, queryString.stringify(formData))
+      .then((res) => {
+        console.log(res);
+        if (res.status === 201) {
+          return Swal.fire({
+            title: "Success",
+            text: (
+              <div>
+                <div>userName: {JSON.stringify(res.data)}</div>
+              </div>
+            ),
+            icon: "success",
+            confirmButtonText: "Done",
+          });
+        } else {
+          return Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: "<a href>Why do I have this issue?</a>",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}></Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up Uptodate in Dr+
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                value={values.firstName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                value={values.lastName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={values.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="registrationUserName"
+                value={values.registrationUserName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={values.password}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
